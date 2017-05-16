66102c6bf5d34c7a81b01987a6ad9f90498e4593
yujiangmin <yujiangmin@gmail.com>
2017-05-15 20:01:15 -0500

    add hello master

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// commentff
 		System.out.println("say hello@");
-		System.out.println("say Hello!");
+		System.out.println("say Hello, my master!");
 	}
 
 }
