a9413dc5d2d142a90335a2b88397349e33b00071
Bessie <Bessie@BessieThink.MissouriState.edu>
2017-05-16 15:20:25 -0500

    add master message

--- Hello/src/Hello2.java
+++ Hello/src/Hello2.java
@@ -6,6 +6,7 @@ public class Hello2 {
 		System.out.println("hello!");
 		System.out.println("hello, again!");
 		System.out.println("hello from author 2!");
+		System.out.println("hello from master author 1!");
 	}
 
 }
