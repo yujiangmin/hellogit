51e0209ac3f0b5d6f69c759852eb6a36b099db03
Bessie <Bessie@BessieThink.MissouriState.edu>
2017-05-16 14:50:25 -0500

    1

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -3,15 +3,15 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// commentff
-<<<<<<< HEAD
+
 		System.out.println("say hello!");
-=======
+
 		System.out.println("say hello@");
 		System.out.println("say Hello!");
 		System.out.println("say Hello, right now!");
 		System.out.println("say Hello, my master!");
 
->>>>>>> branch 'master' of https://github.com/yujiangmin/hellogit.git
+
 	}
 
 }
